dd9c90cd830f1c7c02fa5216d19ca49da802e9e3
Ramses <ramses@ramsesz.be>
2017-03-23 08:50:57 +0100

    add colors to vals

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 var particle = new Particle();
 var token = 'b373b7d323202577e2892b28cbd56464b1b4f7dc';
 var DEVICE_ID = '3f0062000851353531343431';
+var slackURL = 'https://hooks.slack.com/services/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';
 
 var devicesPr = particle.listDevices({auth: token});
+
+var potIsleeg = false;
 
 devicesPr.then(
     function (devices) {
@@ -30,22 +33,79 @@ function ledToggle() {
 
 }
 
-var url = 'https://api.particle.io/v1/devices/' + DEVICE_ID + '/analogvalue?access_token=' + token;
+var url = 'https://api.particle.io/v1/devices/' + DEVICE_ID + '/analogvalue';
 
 var itvl;
+var output = 0;
+var offset = 1200;
 
-function startReading () {
+function getPercentage(readVal) {
+
+
+    var quotient = readVal / offset;
+    quotient = Math.min(1.0, Math.max(0.0, quotient));
+    var k = quotient * quotient * 100;
+
+    return k;
+
+    // var perc = Math.round(readVal / offset * 100);
+    // return perc;
+}
+
+function notifySlack() {
+
+    var data = {
+        text: "WATER BIJVULLEN :facepunch:"
+    };
+
+    $.ajax({
+        type: "POST",
+        url: slackURL,
+        data: JSON.stringify(data),
+        processData: true,
+    });
+}
+
+function startReading() {
+
+    $('#btnToggle').html('KOFFEBOT IS GOING ...');
+    processResult(offset);
+    potIsleeg = false;
+    if (itvl) {
+        clearInterval(itvl);
+    }
     itvl = setInterval(function () {
         var data = {
             access_token: token
         };
 
-        $.getJSON( url, function( data ) {
-                console.log(data);
+        $.getJSON(url, data, function (data) {
+            if (!isNaN(data.result)) {
 
-                $('#readVal').html(data.result);
+
+                processResult(data.result);
+
+
+            }
         });
     }, 1000);
+}
+
+function processResult(dataResult) {
+
+    var perc = getPercentage(dataResult);
+    output = Math.round(perc) + '%';
+    if (perc < 10 && !potIsleeg) {
+        potIsleeg = true;
+        notifySlack();
+        $('#percVal').addClass('leeg');
+        $('#readVal').addClass('leeg');
+    } else {
+        $('#percVal').removeClass('leeg');
+        $('#readVal').removeClass('leeg');
+    }
+    $('#percVal').html(output);
+    $('#readVal').html(dataResult);
 }
 
 
@@ -61,16 +121,5 @@ function ledToggle() {
         });
 
 }
-
-
-// particle.login({username: 'tjoener@gmail.com', password: 'pass'}).then(
-//     function(data) {
-//         token = data.body.access_token;
-//     },
-//     function (err) {
-//         console.log('Could not log in.', err);
-//     }
-// );
-
 
 
